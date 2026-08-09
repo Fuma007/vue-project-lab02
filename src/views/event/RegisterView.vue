96dcaad05647d3edd09f3f0e6f6b594c6556c6a4
Fuma078 <fam0075552@gmail.com>
2026-08-09 20:31:05 +0700

    1.4 commit

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -2,6 +2,7 @@
 import { toRefs } from 'vue'
 import { useRouter } from 'vue-router'
 import { type Event } from '@/types'
+import { useMessageStore } from '@/stores/message';
 
 const props = defineProps<{
   event: Event
@@ -11,11 +12,13 @@ const props = defineProps<{
 const { event } = toRefs(props)
 
 const router = useRouter()
-
+const store = useMessageStore()
 const register = () => {
-  // If the registration API call successful
-  // Push back to the event details view
-  router.push({ name: 'event-detail-view' })
+  store.updateMessage('You are successfully registered for the ${props.event.title}!')
+  setTimeout(() => {
+    store.resetMessage()
+  }, 3000)
+  router.push({ name: 'event-detail-view', params: { id: props.event.id } })
 }
 </script>
 
