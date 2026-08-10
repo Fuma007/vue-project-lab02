b28fb0c447702c1131abfc321b466cb1eb163fb7
Fuma078 <fam0075552@gmail.com>
2026-08-10 09:33:53 +0700

    1.3 & 1.4 commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-// import './assets/main.css'
+import './assets/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,6 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import 'nprogress/nprogress.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
